a25460e73dde44e1f73739ed88759a61e14e3a05
kuroneko <kskkm0228@gmail.com>
2019-10-02 21:42:10 +0900

    モンテカルロ法の利用、試作段階

---
+++ random.py
@@ -0,0 +1,10 @@
+import random
+# モンテカルロ法
+area = []
+
+for i in range(1, 101):
+    for y in range(1, 101):
+        comprehension_1= [i for i in range(1, 101)]
+        area.append(comprehension_1)
+
+print(area)
